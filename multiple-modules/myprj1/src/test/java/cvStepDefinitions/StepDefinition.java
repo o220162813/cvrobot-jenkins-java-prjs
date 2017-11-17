a2b7fce2cef55a10651cb609ceab5c3cc98b11ce
o220162813 <cstephenlin@hotmail.com>
2017-11-18 01:01:26 +1100

    multiple-modules: added initial commit

---
+++ multiple-modules/myprj1/src/test/java/cvStepDefinitions/StepDefinition.java
@@ -0,0 +1,25 @@
+package cvStepDefinitions;
+
+import cucumber.api.java.en.Given;
+import main.Main;
+import org.testng.Assert;
+
+/**
+ * Created by Stephen C-F Lin on 2017/10/8.
+ */
+public class StepDefinition {
+
+    @Given("^Tester logs on to the \"([^\"]*)\" \"([^\"]*)\" system$")
+    public void givenTesterAccess(String varargin1, String varargin2) throws Throwable {
+        System.out.println("Tester access reached");
+        System.out.println(varargin1);
+        System.out.println(varargin2);
+
+        if(varargin1.equalsIgnoreCase("21")) {
+            Assert.fail();
+        }
+
+        Main mytestmain = new Main();
+        mytestmain.main(null);
+    }
+}
---
+++ multiple-modules/myprj2/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Stephen C-F Lin on 2017/11/4.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test message 1 generated from myprj2");
+        System.out.println("This is a test message 2 generated from myprj2");
+    }
+
+}
